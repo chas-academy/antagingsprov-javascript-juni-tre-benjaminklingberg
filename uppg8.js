c3f74459f0aebf3c83b98f9bfa5748e768a629a6
benjaminklingberg <benne.klingberg@gmail.com>
2025-06-29 15:16:07 +0200

    Update uppg8.js

--- uppg8.js
+++ uppg8.js
@@ -10,8 +10,21 @@ function uppg8(){
 //  över 30 år som skrivs ut i konsolen
 
 // anropa funktionen och skicka med arrayen som argument
+let personer = [
+  { name: "Benjamin", age: 28 },
+  { name: "Sven", age: 35 },
+  { name: "Noah", age: 42 },
+  { name: "Hanna", age: 25 },
+  { name: "Sara", age: 31 }
+];
 
-
+function skrivUtPersonerOver30(array) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i].age > 30) {
+      console.log(array[i].name);
+    }
+  }
 }
-
+  
+skrivUtPersonerOver30(personer);
 module.exports = { uppg8 };
