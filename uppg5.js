394028832183d0f8b7e6e3e7ab624bf97af491e9
benjaminklingberg <benne.klingberg@gmail.com>
2025-06-29 15:07:40 +0200

    Update uppg5.js

--- uppg5.js
+++ uppg5.js
@@ -3,9 +3,14 @@ function uppg5() {
     // skriv en if-sats som jämför två tal
     // om det första talet är större än det andra, skriv ut "Det första talet är större än det andra"
     // annars skriv ut "Det andra talet är större än det första"
-
-
-
+let tal1 = 12;
+let tal2 = 8;
+    
+if (tal1 > tal2) {
+  console.log("Det första talet är större än det andra");
+} else {
+  console.log("Det andra talet är större än det första");
+}
 }
 
 module.exports = { uppg5 };
